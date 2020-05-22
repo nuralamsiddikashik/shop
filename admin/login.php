c7d2636d3cf4c85f76f2b1a65c12812f9c404d33
nuralamsiddikashik <nuralamsiddikashik@gmail.com>
2020-05-22 20:23:37 +0600

    Ecommerce Complete Solution

---
+++ admin/login.php
@@ -0,0 +1,91 @@
+<?php
+session_start();
+
+if ( isset( $_SESSION['login'] ) == true ) {
+    header( "Location: index.php" );
+}
+
+require_once "connection.php";
+//require_once "functions.php";
+
+if ( isset( $_POST['login_dashboard'] ) ) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    if ( empty( $username ) ) {
+        echo "<div class='Field-error'>User name can not be empty</div>";
+
+    } else if ( empty( $password ) ) {
+        echo "<div class='field-error'>Password can not be empty</div>";
+
+    } else {
+        //$password         = md5( $password );
+        $loginQuery       = "SELECT * FROM `admin_user` WHERE username='{$username}' AND password='{$password}'";
+        $successfulyLogin = mysqli_query( $connection, $loginQuery );
+        $userNameRowCount = mysqli_num_rows( $successfulyLogin );
+
+        if ( $userNameRowCount > 0 ) {
+            session_start();
+            $_SESSION['ADMIN_USER'] = $username;
+            $_SESSION['login']      = true;
+            header( "Location: index.php" );
+        } else {
+            echo "Something Worng";
+        }
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>eShop Login</title>
+  <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic">
+  <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.css">
+  <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/milligram/1.3.0/milligram.css">
+
+  <style>
+
+      body{
+          margin-top: 30px;
+      }
+      .login-form {
+          background: #fff;
+          padding: 70px;
+          box-shadow: 45px 63px 73px 64px #ddd;
+      }
+  </style>
+
+</head>
+<body>
+    <div class="container">
+        <div class="login-form">
+
+            <div class="row">
+                <div class="column column-60 column-offset-20">
+                    <h1>eShop Login</h1>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="column column-60 column-offset-20">
+                    <!-- <?php if ( isset( $msg ) ) {echo $msg;}?> -->
+                    <form action="login.php" method="post">
+                        <label>User Name</label>
+                        <input type="text" name="username" id="username">
+
+                        <label>Password</label>
+                        <input type="password" name="password" id="password">
+
+                        <input type="submit" name="login_dashboard" value="Login">
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ admin/logout.php
@@ -0,0 +1,7 @@
+<?php
+
+session_start();
+session_destroy();
+$_SESSION["login"] = false;
+header( "Location: login.php" );
+?>
